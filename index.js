889407dd7f5db49471d0c3dce72f413539465329
Miriam. C <miriam.clemente25@compscihigh.org>
2025-03-24 19:10:19 +0000

    gdgdg

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Install EJS, Express, and MongoDB in Terminal
 
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 const app = express();
 
@@ -15,11 +15,11 @@ app.set("view engine", "ejs");
 app.use(express.json());
 
 const countrySchema = new mongoose.Schema({
-  country: { type: String },
-  flagURL: { type: String },
-  population: { type: Number },
-  officialLanguage: { type: String },
-  hasNuclearWeapons: { type: Boolean },
+  country: { type: String, required: true },
+  flagURL: { type: String, required: true },
+  population: { type: Number, required: true },
+  officialLanguage: { type: String, required: true},
+  hasNuclearWeapons: { type: Boolean, required: true }
 });
 
 const Country = mongoose.model("Country", countrySchema, "Countries");
@@ -27,8 +27,24 @@ const Country = mongoose.model("Country", countrySchema, "Countries");
 // Create a POST route for "/add/country" that adds a country using the request body (3 points)
 // Use postman to add at least THREE different countries
 
+app.post("/add/country", async (req, res) => {
+  const countryOne = await new Add ({
+    country: { type: String },
+    flagURL: { type: String },
+    population: { type: Number },
+    officialLanguage: { type: String },
+    hasNuclearWeapons: { type: Boolean }
+    }) .save()
+    res.json(countryOne)
+});
+
 
 // Create a GET route for "/" that renders countries.ejs with every country from the Countries collection (1 point)
+
+app.get("/", async (req, res) => {
+  const all = await all.find({ })
+  res.render("countries.ejs", {all})
+})
 
 
 // Go to countries.ejs and follow the tasks there (2 points)
@@ -46,7 +62,7 @@ const Country = mongoose.model("Country", countrySchema, "Countries");
 async function startServer() {
   
     // add your SRV string with a database called countries
-  await mongoose.connect("...");
+  await mongoose.connect("mongodb+srv://CSH:CSH2025@cluster0.6uo1d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
   app.listen(3000, () => {
     console.log("Server is running");
